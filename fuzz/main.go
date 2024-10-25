2704692cfd290805b1e12d861f17d9bd404ee09f
Ethan Wickman <esw@halfangle.com>
2024-10-24 19:12:16 -0700

    tutorial 7 fuzzing

---
+++ fuzz/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"errors"
+	"unicode/utf8"
+)
+
+func main() {
+	input := "The quick brown fox jumped over the lazy dog"
+	rev, revErr := Reverse(input)
+	doubleRev, doubleRevErr := Reverse(rev)
+	fmt.Printf("Original: %q\n", input)
+	fmt.Printf("Rev: %q, err: %v\n", rev, revErr)
+	fmt.Printf("Double rev: %q, err: %v\n", doubleRev, doubleRevErr)
+}
+
+
+func Reverse(s string) (string, error) {
+	if !utf8.ValidString(s) {
+		return s, errors.New("input is not valid UTF-8")
+	}
+	b := []rune(s)
+	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
+		b[i], b[j] = b[j], b[i]
+	}
+	return string(b), nil
+}
