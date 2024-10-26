09c8492104b83814779126b26c8de23891c26635
Ethan Wickman <esw@halfangle.com>
2024-10-26 10:40:05 -0700

    maps

---
+++ learnGoWithTests/maps/dictionary.go
@@ -0,0 +1,60 @@
+package main
+
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(key string) (string, error) {
+	definition, ok := d[key]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(key, val string) error {
+	_, err := d.Search(key)
+
+	switch err {
+	case ErrNotFound:
+		d[key] = val
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(key, val string) error {
+	_, err := d.Search(key)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[key] = val
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Delete(key string) {
+	delete(d, key)
+}
+
+func Search(dictionary map[string]string, key string) string {
+	return dictionary[key]
+}
